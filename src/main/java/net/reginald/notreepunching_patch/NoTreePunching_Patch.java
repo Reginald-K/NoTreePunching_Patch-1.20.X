f4ffd7d71714cbb91751af2b91920bf2d23fdfe3
Reginald_K <d.k.n@gmx.de>
2026-01-11 08:53:22 +0100

    Deleted auto generated comments

--- src/main/java/net/reginald/notreepunching_patch/NoTreePunching_Patch.java
+++ src/main/java/net/reginald/notreepunching_patch/NoTreePunching_Patch.java
@@ -13,26 +13,20 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(NoTreePunching_Patch.MODID)
 public class NoTreePunching_Patch
 {
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "notreepunching_patch";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public NoTreePunching_Patch(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
     }
@@ -42,20 +36,17 @@ public class NoTreePunching_Patch
 
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
 
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
